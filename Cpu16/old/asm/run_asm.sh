2a78a6b35be946ac0dde2859a53c825bd029f3c5
Serhii Zashchelkin <serg.zz@ukr.net>
2024-08-15 17:54:10 +0200

    Updates

--- Cpu16/old/asm/run_asm.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-/home/sergzz/serg/Rider/Cpu16Assembler/Cpu16Assembler/bin/Debug/net8.0/Cpu16Assembler $*
--- Cpu16/old/build_verilator.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-verilator --binary --trace --top cpu16_tb --trace cpu16.v test.v alu.v idecoder.v
