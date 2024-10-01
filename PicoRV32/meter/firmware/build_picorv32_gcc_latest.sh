ba37e7bd602abfeda19f120ce9991c5035a8121b
Serhii Zashchelkin <serg.zz@ukr.net>
2024-10-01 19:12:21 +0200

    Updates

--- PicoRV32/meter/firmware/build_picorv32_gcc_latest.sh
+++ PicoRV32/meter/firmware/build_picorv32_gcc_latest.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-/opt2/riscv32i_latest/bin/riscv32-unknown-elf-gcc -specs=nosys.specs -specs=nano.specs -nodefaultlibs -nostdlib -nostartfiles -O3 -T picorv32.ld start.S $*
+/opt2/riscv32i_latest/bin/riscv32-unknown-elf-gcc -specs=nosys.specs -specs=nano.specs -nodefaultlibs -nostdlib -nostartfiles -O3 -T picorv32.ld src/start.S $*
