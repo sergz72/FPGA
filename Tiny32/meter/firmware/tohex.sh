7ecb0b08ebe21a45de46c4e8c7672be5bc01ba5e
Serhii Zashchelkin <serg.zz@ukr.net>
2024-10-03 20:53:08 +0200

    Meter project

---
+++ Tiny32/meter/firmware/tohex.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+cd asm
+
+rm data*.hex
+
+/opt2/riscv32i/bin/riscv32-unknown-elf-objdump -d $1 > $1.asmdump
+/opt2/riscv32i/bin/riscv32-unknown-elf-objdump -s $1 > $1.datadump
+/home/sergzz/serg/Rider/DumpToHex/DumpToHex/bin/Debug/net8.0/DumpToHex 2 $1.asmdump $1.datadump
+
+cd ..
