f04854cc047afc93ab7b66e1a78d9c815e2d4efd
Serhii Zashchelkin <serg.zz@ukr.net>
2024-10-19 20:20:20 +0200

    tiny32 v3 update

--- Tiny32/test/rv32i_tests/build_verilator_v3.sh
+++ Tiny32/test/rv32i_tests/build_verilator_v3.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-verilator $* -I../../v3 --binary --trace --top tiny32_tb ../../v3/tiny32.v test.v ../../../common/div.v ../../v3/instruction_decoder.v
+verilator $* -I../../v3 --binary --trace --top tiny32_tb ../../v3/tiny32.v test.v ../../../common/div.v
