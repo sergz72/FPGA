9f53a52af13f5ed80cd5113d807e00f8c56ed4ba
Serhii Zashchelkin <serg.zz@ukr.net>
2024-10-21 20:39:37 +0200

    Added error checking

--- Tiny32/test/rv32i_tests/build_gcc.sh
+++ Tiny32/test/rv32i_tests/build_gcc.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
 /opt2/riscv32i/bin/riscv32-unknown-elf-gcc -march=rv32im -specs=nosys.specs -specs=nano.specs -nodefaultlibs -nostdlib -nostartfiles -o asm/a.out -T ldscript.ld start.S $*
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  exit $retVal
+fi
 
 cd asm
 
@@ -9,5 +13,9 @@ rm data*.hex
 /opt2/riscv32i/bin/riscv32-unknown-elf-objdump -d a.out > a.out.asmdump
 /opt2/riscv32i/bin/riscv32-unknown-elf-objdump -s a.out > a.out.datadump
 DumpToHex 2 a.out.asmdump a.out.datadump
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  exit $retVal
+fi
 
 cd ..
