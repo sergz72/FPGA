f373bf782836ea68fca9418fd303bd4bd8ade8a1
Serhii Zashchelkin <serg.zz@ukr.net>
2024-10-25 18:27:41 +0200

    Updates

--- PicoRV32/meter/test/build_verilator.sh
+++ PicoRV32/meter/test/build_verilator.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-verilator $* -I.. --binary --trace --top test ../main.v ../test.v ../../picorv32.v ../../../common/uart1.v ../../../common/uart_fifo.v ../../../common/fifo.v
+verilator $* -I.. --binary --trace --top test ../main.v ../test.v ../../picorv32.v ../../../common/uart1.v ../../../common/uart_fifo.v ../../../common/fifo.v ../../../common/timer.v ../../../common/time_counter.v
