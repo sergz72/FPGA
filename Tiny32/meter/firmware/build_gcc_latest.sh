dc2034503734dc15eac13d37655db352df51d2f5
Serhii Zashchelkin <serg.zz@ukr.net>
2024-12-05 18:07:23 +0100

    Updates

--- Tiny32/meter/firmware/build_gcc_latest.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-/opt2/riscv32i_latest/bin/riscv32-unknown-elf-gcc -specs=nosys.specs -specs=nano.specs -nodefaultlibs -nostdlib -nostartfiles -O3 -T ldscript.ld -o asm/a.out src/start.S $*
--- Tiny32/meter/firmware/tohex.sh
+++ Tiny32/meter/firmware/tohex.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
+rm asm/*data*.hex
+
+/opt2/riscv32i/bin/riscv32-unknown-elf-objdump -d $1 > asm/$1.asmdump
+/opt2/riscv32i/bin/riscv32-unknown-elf-objdump -s $1 > asm/$1.datadump
+
 cd asm
 
-rm data*.hex
-
-/opt2/riscv32i/bin/riscv32-unknown-elf-objdump -d $1 > $1.asmdump
-/opt2/riscv32i/bin/riscv32-unknown-elf-objdump -s $1 > $1.datadump
-/home/sergzz/serg/Rider/DumpToHex/DumpToHex/bin/Debug/net8.0/DumpToHex 2 $1.asmdump $1.datadump
+DumpToHex 2 $1.asmdump $1.datadump
 
 cd ..
