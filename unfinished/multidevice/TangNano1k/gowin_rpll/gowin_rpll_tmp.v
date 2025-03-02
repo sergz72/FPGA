d0c63747253587e55e5fd11580fe3ab11d6978d2
Serhii Zashchelkin <serg.zz@ukr.net>
2025-03-02 11:04:58 +0100

    Updates

---
+++ unfinished/multidevice/TangNano1k/gowin_rpll/gowin_rpll_tmp.v
@@ -0,0 +1,17 @@
+//Copyright (C)2014-2024 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.11
+//Part Number: GW1NZ-LV1QN48C6/I5
+//Device: GW1NZ-1
+//Created Time: Fri Feb 28 21:39:50 2025
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_rPLL your_instance_name(
+        .clkout(clkout), //output clkout
+        .clkin(clkin) //input clkin
+    );
+
+//--------Copy end-------------------
