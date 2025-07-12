2880f6ec7fe72f8031e71f8f87567e30a796ff14
Serhii Zashchelkin <serg.zz@ukr.net>
2025-07-12 10:35:44 +0200

    Updates

--- common/ws2812b.v
+++ common/ws2812b.v
@@ -19,22 +19,32 @@ module ws2812b
     reg [8:0] counter;
     reg [COUNT_BITS:0] current_address;
     reg busy = 0;
+    reg start = 0;
+
+    always @(negedge clk) begin
+        mem_ready <= mem_valid;
+        if (!nreset)
+            start <= 0;
+        else if (mem_valid) begin
+            mem[address] <= {g, r, b};
+            if (address == MAX_ADDRESS)
+                start <= 1;
+        end
+        else if (busy)
+            start <= 0;
+    end
 
     always @(posedge clk) begin
-        mem_ready <= mem_valid;
         if (!nreset) begin
             busy <= 0;
             dout <= 0;
         end
         else begin
-            if (mem_valid) begin
-                mem[address] <= {g, r, b};
-                if (address == MAX_ADDRESS) begin
-                    current_address <= 0;
-                    bit_counter <= 23;
-                    busy <= 1;
-                    counter <= COUNTER_MAX;
-                end
+            if (start) begin
+                busy <= 1;
+                current_address <= 0;
+                bit_counter <= 23;
+                counter <= COUNTER_MAX;
             end
             else begin
                 if (busy) begin
